e71b7b27253eb2f1f1de6e349589ccc3c6a84a75
Lilysss02 <shkrkmtd219@gmail.com>
2019-04-11 09:41:07 +0000

    [Create]お気に入り登録機能

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,22 @@
 class FavoritesController < ApplicationController
+	def index
+		@favorites = Favorite.where(user_id: current_user.id)
+	end
+
+	def create
+		post = Post.find(params[:post_id])
+		favorite = current_user.favorites.new(post_id: post.id)
+		added_favorite = current_user.favorites.find_by(post_id: favorite.post.id)
+  		# 存在しなければ新たにお気に入りに追加
+  		if  added_favorite == nil
+  			favorite.save
+  		else
+  		end
+    end
+
+	def destroy
+		post = Post.find(params[:post_id])
+        favorite = current_user.favorites.find_by(post_id: post.id)
+        favorite.destroy
+	end
 end
