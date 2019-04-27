df65a809dd3ec89b5f9a3c1cae6f86af913ff555
Lilysss02 <shkrkmtd219@gmail.com>
2019-04-27 00:22:48 +0000

    [Update]マイグレーションファイルの編集

--- db/migrate/20190408062938_create_posts.rb
+++ db/migrate/20190408062938_create_posts.rb
@@ -1,6 +1,6 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
-    create_table :posts do |t|
+    create_table :posts, :options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC' do |t|
       t.string :title
       t.text :body
       t.date :used_at
@@ -10,6 +10,10 @@ class CreatePosts < ActiveRecord::Migration[5.2]
       t.references :price, foreign_key: true
       t.references :user, foreign_key: true
       t.integer :status, default: 0, null: false, limit: 1
+      t.string :shop_map
+      t.string :latitude
+      t.string :longitude
+      t.string :review
 
       t.timestamps
     end
