2799030398e5732f06befb38f1632a958907610a
Lilysss02 <shkrkmtd219@gmail.com>
2019-04-28 21:35:46 +0000

    Revert "[Fix]"
    This reverts commit b188349966a3a96955b2e5d295318d953961b293, reversing
    changes made to 4991b41258bf8d4c10ca77f8007dd9469785d64f.

--- db/migrate/20190408062349_create_prices.rb
+++
@@ -1,9 +0,0 @@
-class CreatePrices < ActiveRecord::Migration[5.2]
-  def change
-    create_table :prices, :options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC' do |t|
-      t.string :price_range
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20190408063937_create_posts_tags.rb
+++
@@ -1,8 +0,0 @@
-class CreatePostsTags < ActiveRecord::Migration[5.2]
-  def change
-    create_table :posts_tags, :id => false, :options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC' do |t|
-      t.references :post, index: true, foreign_key: true
-      t.references :tag, index: true, foreign_key: true
-    end
-  end
-end
