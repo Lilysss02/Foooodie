09a063b01414c6f2c356ab6396a6a2d5a01de002
Lilysss02 <shkrkmtd219@gmail.com>
2019-04-29 03:51:09 +0000

    [Create]priceデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+CSV.foreach('db/prices.csv') do |info|
+  Price.create(:price_range => info[0])
+end
